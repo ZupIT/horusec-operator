14b0b090220e5e1d042147949af959296ecfb3a8
wilian <wilian.silva@zup.com.br>
2021-05-26 14:38:52 -0300

    Fix operator to v2alpha1

--- api/v2alpha1/groupversion_info.go
+++ api/v2alpha1/groupversion_info.go
@@ -15,7 +15,7 @@
 // Package v2 contains API Schema definitions for the install v2 API group
 //+kubebuilder:object:generate=true
 //+groupName=install.horusec.io
-package v2
+package v2alpha1
 
 import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -24,7 +24,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "install.horusec.io", Version: "v2"}
+	GroupVersion = schema.GroupVersion{Group: "install.horusec.io", Version: "v2alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
