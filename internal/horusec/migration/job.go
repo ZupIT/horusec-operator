222ab5231116371583d5cf398d8a4c638663e15a
Tiago Angelo <tiago.angelo@zup.com.br>
2021-06-04 06:28:51 -0300

    Recreate migration Job if it changes

--- internal/horusec/migration/job.go
+++ internal/horusec/migration/job.go
@@ -10,21 +10,24 @@ import (
 )
 
 func NewJob(resource *v2alpha1.HorusecPlatform) batchv1.Job {
+	var terminationPeriod int64 = 30
 	return batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      fmt.Sprintf("%s-platform-migration", resource.GetName()),
-			Namespace: resource.GetNamespace(),
-			Labels:    resource.GetDefaultLabel(),
+			GenerateName: fmt.Sprintf("%s-platform-migration-", resource.GetName()),
+			Namespace:    resource.GetNamespace(),
+			Labels:       resource.GetDefaultLabel(),
 		},
 		Spec: batchv1.JobSpec{
 			Template: corev1.PodTemplateSpec{
 				Spec: corev1.PodSpec{
-					RestartPolicy: corev1.RestartPolicyNever,
+					RestartPolicy:                 corev1.RestartPolicyNever,
+					TerminationGracePeriodSeconds: &terminationPeriod,
 					Containers: []corev1.Container{
 						{
-							Name:    "horusec-database-migration",
-							Image:   resource.GetDatabaseMigrationImage(),
-							Command: []string{"migrate.sh"},
+							Name:            "horusec-database-migration",
+							Image:           resource.GetDatabaseMigrationImage(),
+							ImagePullPolicy: corev1.PullIfNotPresent,
+							Command:         []string{"migrate.sh"},
 							Env: []corev1.EnvVar{
 								resource.NewEnvFromSecret("HORUSEC_DATABASE_USERNAME", resource.GetGlobalDatabaseUsername()),
 								resource.NewEnvFromSecret("HORUSEC_DATABASE_PASSWORD", resource.GetGlobalDatabasePassword()),
--- internal/inventory/job.go
+++ internal/inventory/job.go
@@ -2,8 +2,29 @@ package inventory
 
 import (
 	"fmt"
+
+	"github.com/google/go-cmp/cmp"
 	batchv1 "k8s.io/api/batch/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	ignoredJobFields = [...]string{
+		"ObjectMeta.Labels",
+		"Spec.Containers.TerminationMessagePath",
+		"Spec.Containers.TerminationMessagePolicy",
+		"Spec.DNSPolicy",
+		"Spec.SchedulerName",
+		"Spec.SecurityContext",
+	}
+	jobDiffOptions = cmp.FilterPath(func(path cmp.Path) bool {
+		for _, p := range ignoredJobFields {
+			if p == path.String() {
+				return true
+			}
+		}
+		return false
+	}, cmp.Ignore())
 )
 
 //nolint:gocritic, funlen // improve in the future
@@ -14,29 +35,11 @@ func ForJobs(existing, desired []batchv1.Job) Object {
 
 	for k, v := range mcreate {
 		if t, ok := mdelete[k]; ok {
-			tp := t.DeepCopy()
-			//
-			//if reflect.ValueOf(v.Spec.Template).IsZero() && !reflect.ValueOf(tp.Spec.Template).IsZero() {
-			//	v.Spec.Template = tp.Spec.Template
-			//}
-			//if v.Spec.Selector == nil && tp.Spec.Selector != nil {
-			//	v.Spec.Selector = tp.Spec.Selector
-			//}
-
-			//tp.Spec = v.Spec
-			tp.ObjectMeta.OwnerReferences = v.ObjectMeta.OwnerReferences
-
-			for k, v := range v.ObjectMeta.Annotations {
-				tp.ObjectMeta.Annotations[k] = v
+			diff := cmp.Diff(v.Spec.Template, t.Spec.Template, jobDiffOptions)
+			if diff == "" {
+				delete(mcreate, k)
+				delete(mdelete, k)
 			}
-
-			for k, v := range v.ObjectMeta.Labels {
-				tp.ObjectMeta.Labels[k] = v
-			}
-
-			update = append(update, tp)
-			delete(mcreate, k)
-			delete(mdelete, k)
 		}
 	}
 
@@ -48,10 +51,13 @@ func ForJobs(existing, desired []batchv1.Job) Object {
 }
 
 //nolint:gocritic // improve in the future
-func jobMap(deps []batchv1.Job) map[string]batchv1.Job {
+func jobMap(jobs []batchv1.Job) map[string]batchv1.Job {
 	m := map[string]batchv1.Job{}
-	for _, d := range deps {
-		m[fmt.Sprintf("%s.%s", d.Namespace, d.Name)] = d
+	for _, j := range jobs {
+		labels := j.GetLabels()
+		component := labels["app.kubernetes.io/component"]
+		name := labels["app.kubernetes.io/name"]
+		m[fmt.Sprintf("%s.%s.%s", j.Namespace, component, name)] = j
 	}
 	return m
 }
