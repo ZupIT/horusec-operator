01a6be3678a4be3578a58a798b885f71d9905224
Tiago Angelo <tiago.angelo@zup.com.br>
2021-06-08 10:58:56 -0300

    Moving Ingresses creation

---
+++ internal/resources/ingress.go
@@ -0,0 +1,22 @@
+package resources
+
+import (
+	"fmt"
+
+	"github.com/ZupIT/horusec-operator/api/v2alpha1"
+	"github.com/ZupIT/horusec-operator/internal/horusec/ingress"
+	networkingv1 "k8s.io/api/networking/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+func (b *Builder) IngressFor(resource *v2alpha1.HorusecPlatform) ([]networkingv1.Ingress, error) {
+	var desiredList []networkingv1.Ingress
+	if !resource.GetAllIngressIsDisabled() {
+		desired := ingress.NewIngress(resource)
+		if err := controllerutil.SetControllerReference(resource, &desired, b.scheme); err != nil {
+			return nil, fmt.Errorf("failed to set ingress %q owner reference: %v", desired.GetName(), err)
+		}
+		desiredList = append(desiredList, desired)
+	}
+	return desiredList, nil
+}
