25773e8fd7685b6b9f42e368b49d966187817625
Tiago Angelo <tiago.angelo@zup.com.br>
2021-06-08 15:11:58 -0300

    Kubernetes client providers

--- cmd/app/wire_gen.go
+++ cmd/app/wire_gen.go
@@ -8,6 +8,7 @@ package main
 import (
 	"github.com/ZupIT/horusec-operator/controllers"
 	"github.com/ZupIT/horusec-operator/internal/horusec"
+	"github.com/ZupIT/horusec-operator/internal/k8s"
 	"github.com/ZupIT/horusec-operator/internal/resources"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
@@ -22,8 +23,8 @@ func newHorusecPlatformReconciler(mgr manager.Manager) (*controllers.HorusecPlat
 	runtimeScheme := extractScheme(mgr)
 	builder := resources.NewBuilder(runtimeScheme)
 	client := extractClient(mgr)
-	service := horusec.NewService(client)
-	adapterFactory := horusec.NewAdapterFactory(builder, service)
+	k8sClient := k8s.NewClient(client)
+	adapterFactory := horusec.NewAdapterFactory(builder, k8sClient)
 	horusecPlatformReconciler := controllers.NewHorusecPlatformReconciler(adapterFactory)
 	return horusecPlatformReconciler, nil
 }
