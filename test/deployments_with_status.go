c1a5a00c4157fbd06507a156211dfa93ea7ea783
Tiago Angelo <tiago.angelo@zup.com.br>
2021-06-15 14:03:37 -0300

    Use Kubernetes utility function for YAML read

--- test/deployments_with_status.go
+++ test/deployments_with_status.go
@@ -1,24 +1,23 @@
 package test
 
 import (
-	"io/ioutil"
+	"os"
 	"path"
 	"runtime"
 
-	"gopkg.in/yaml.v3"
-
 	apps "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
 func DeploymentsWithStatus() ([]apps.Deployment, error) {
 	_, filepath, _, _ := runtime.Caller(0)
-	data, err := ioutil.ReadFile(path.Join(path.Dir(filepath), "data/deployments_with_status.yaml"))
+	data, err := os.Open(path.Join(path.Dir(filepath), "data/deployments_with_status.yaml"))
 	if err != nil {
 		return nil, err
 	}
 
 	var deploys apps.DeploymentList
-	if err = yaml.Unmarshal(data, &deploys); err != nil {
+	if err = yaml.NewYAMLOrJSONDecoder(data, 100).Decode(&deploys); err != nil {
 		return nil, err
 	}
 
