40370278097c98866f091a9bac72a37b0766d1d6
wilian <wilian.silva@zup.com.br>
2021-09-15 07:56:10 -0300

    =[skip ci] Update versioning files

--- api/v2alpha1/component_global.go
+++ api/v2alpha1/component_global.go
@@ -36,7 +36,7 @@ func (h *HorusecPlatform) GetDefaultLabel() map[string]string {
 }
 
 func (h *HorusecPlatform) GetLatestVersion() string {
-	return "v2.16.5"
+	return "v2.17.0"
 }
 
 func (h *HorusecPlatform) GetGlobalDatabaseLogMode() string {
--- api/v2alpha1/database_migration.go
+++ api/v2alpha1/database_migration.go
@@ -49,7 +49,7 @@ func (in *HorusecPlatform) GetGlobalDatabaseMigrationImageTag() string {
 	if tag != "" {
 		return tag
 	}
-	return "v2.16.5"
+	return "v2.17.0"
 }
 
 func (h *HorusecPlatform) GetDatabaseMigrationLabels() map[string]string {
